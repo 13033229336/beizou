120021ba2116eaba870df90b1e3439988e1a3448
13033229336 <874609947@qq.com>
2020-05-24 13:35:29 +0800

    vuepress blog

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    title: '北走的博客',  
+    head: [
+        ['link', { rel: 'icon', href: '/logo.png' }],    
+    ],
+    themeConfig: {
+        nav: [
+            { text: '主页', link: '/' },
+            { text: '博文',
+              items: [
+                { text: '个人学习', link: '/study/' },
+                { text: '学习笔记', link: '/reading/' },
+                { text: '优质文章', link: '/article/' },
+                { text: '面试经典', link: '/interview/' },
+              ] 
+            },
+            { text: '关于', link: '/about/' },
+            { text: 'Github', link: 'https://github.com/13033229336' },
+        ],
+        sidebar: 'auto',     //侧边栏
+        sidebarDepth: 2,
+        lastUpdated: 'Last Updated', 
+    },
+}
